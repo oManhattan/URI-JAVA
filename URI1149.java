992d6227d2a89633e8dea69fb4010a02e83b0e66
Manhattan <matheusbatuque@gmail.com>
2022-03-03 00:38:10 -0300

    Update URI1149.java

--- URI1149.java
+++ URI1149.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class URI1149 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         
+        int a, n;
+
+        a = sc.nextInt();
+        n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            a = a + i;
+        }
+
+        System.out.println(a);
+
+        sc.close();
     }
 }
